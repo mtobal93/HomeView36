67845fcd6cf34e77874bfe26000bb9c686843c16
mtobal93 <mtobal93@gmail.com>
2025-05-05 12:22:30 +0300

    integrating App Write for backend

--- src/appwrite.js
+++ src/appwrite.js
@@ -2,7 +2,7 @@ const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
-export const showMe = async () => {
-    return console.log(PROJECT_ID, DATABASE_ID, COLLECTION_ID);
+export const updateSearchCount = async () => {
+
     
 }
