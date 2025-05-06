7868d8ccb0f4c81ffa2bbc1c07ee4c8d20c99e13
mtobal93 <mtobal93@gmail.com>
2025-05-06 16:37:07 +0300

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./components/search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import { useDebounce } from "react-use";
-import { updateSearchCount } from "./appwrite.js";
+import { updateSearchCount, getTendingMovies } from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -19,10 +19,11 @@ const App = () => {
 	const [searchTerm, setSearchTerm] = React.useState("");
 	const [errorMessage, setErrorMessage] = useState("");
 	const [movieList, setMovieList] = useState([]);
+	const [trendingMovies, setTrendingMovies] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [debouncedSearchTerm, setDebouncedSearchTerm] = useState()
 
-	useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+	useDebounce(() => setDebouncedSearchTerm(searchTerm), 1000, [searchTerm]);
 
 	const fetchMovies = async (query = "") => {
 		setIsLoading(true);
@@ -41,9 +42,16 @@ const App = () => {
 			if (data.Response === "False") {
 				setErrorMessage(data.Error || "Failed to fetch movies");
 				setMovieList([]);
+				return;
 			}
 
 			setMovieList(data.results || []);
+
+			if (query && data.results.length > 0) {
+				// Update search count in Appwrite
+				await updateSearchCount(query, data.results[0]);
+			}
+
 		} catch (error) {
 			console.error(`Error fetching movies: ${error}`);
 		} finally {
@@ -51,9 +59,24 @@ const App = () => {
 		}
 	};
 
+	const loadTrendingMovies = async () => {
+		try {
+
+			const movies = await getTendingMovies();
+			setTrendingMovies(movies);
+
+		} catch (error) {
+			console.error(`Error fetching trending movies: ${error}`);
+		}
+	}
+
 	useEffect(() => {
 		fetchMovies(debouncedSearchTerm);
 	}, [debouncedSearchTerm]);
+
+	useEffect(() => {
+		loadTrendingMovies();
+	},[]);
 
 	return (
 		<main>
@@ -71,8 +94,21 @@ const App = () => {
 						/>
 					</header>
 
+					{trendingMovies.length > 0 && (
+						<section className="trending">
+							<h2>Trending Movies</h2>
+							<ul>
+								{trendingMovies.map((movie, index) => (
+									<li key={movie.$id}>
+										<p>{index+1}</p>
+										<img src={movie.poster_url} alt={movie.title} />
+									</li>
+								))}
+							</ul>
+						</section>
+					)}
 					<section className="all-movies">
-						<h2 className="mt-[40px]">All Movies</h2>
+						<h2>All Movies</h2>
 
 						{isLoading ? (
 							<Spinner />
